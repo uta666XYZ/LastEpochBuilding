5fd4633811fdc49d476aba3056cdd3dd5b912769
Musholic <musholic.tbg@gmail.com>
2024-07-17 16:58:59 +0200

    Regenerated all game extracts for version 1.1

--- src/GameVersions.lua
+++ src/GameVersions.lua
@@ -1,13 +1,13 @@
 -- Game versions
 ---Default target version for unknown builds and builds created before 3.0.0.
-legacyTargetVersion = "1_0"
+legacyTargetVersion = "1_1"
 ---Default target for new builds and target to convert legacy builds to.
-liveTargetVersion = "1_0"
+liveTargetVersion = "1_1"
 
 -- Skill tree versions
 ---Added for convenient indexing of skill tree versions.
 ---@type string[]
-treeVersionList = { "1_0", }
+treeVersionList = { "1_1", }
 --- Always points to the latest skill tree version.
 latestTreeVersion = treeVersionList[#treeVersionList]
 ---Tree version where multiple skill trees per build were introduced to PoBC.
@@ -15,9 +15,9 @@ defaultTreeVersion = treeVersionList[2]
 ---Display, comparison and export data for all supported skill tree versions.
 ---@type table<string, {display: string, num: number, url: string}>
 treeVersions = {
-	["1_0"] = {
-		display = "1.0",
-		num = 1.0,
+	["1_1"] = {
+		display = "1.1",
+		num = 1.1,
 		url = "",
 	},
 }
