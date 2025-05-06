b7f125804a6fc97db309007a755c0b4f6982037a
Musholic <musholic.tbg@gmail.com>
2025-05-06 18:12:49 +0200

    [extract_scripts] Test using melon loader mod for hot reload and debugging supprt

---
+++ newGameFileExtractScripts2/Core.cs
@@ -0,0 +1,25 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(MelonLoaderMod1.Core), "MelonLoaderMod1", "0.1.0", "Musholic", null)]
+[assembly: MelonGame("Eleventh Hour Games", "Last Epoch")]
+
+namespace MelonLoaderMod1
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+            int count = GlobalTreeData.get().passiveTrees.Count;
+            LoggerInstance.Msg("Got count: " + count);
+
+            Application.Quit();
+        }
+        public override void OnUpdate()
+        {
+            LoggerInstance.Msg("OnUpdate!");
+        }
+    }
+}
