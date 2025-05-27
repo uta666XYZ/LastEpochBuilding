66a71d9405ba29d5e2bf4537924ec217b635a378
Musholic <musholic.tbg@gmail.com>
2025-05-27 21:01:18 +0200

    [gameFileExtractMod] Extract skills support WIP

--- gameFileExtractMod/Core.cs
+++ gameFileExtractMod/Core.cs
@@ -47,6 +47,7 @@ namespace PobfleExtractor
             {
                 Logger.Msg("Starting extract...");
                 _genId = targetId;
+                Skills.Extract();
                 Uniques.Extract();
                 Mods.Extract();
                 TreeData.Extract();
