bd1df00433936008a7f925c5fd52ef3fcccd5e5c
Musholic <musholic.tbg@gmail.com>
2025-07-19 20:34:27 +0200

    Support for ward decay threshold, ward per second and ward retention

--- src/Modules/CalcPerform.lua
+++ src/Modules/CalcPerform.lua
@@ -1129,7 +1129,7 @@ function calcs.perform(env, fullDPSSkipEHP)
 	end
 
 	-- Defence/offence calculations
-	for _, stat in ipairs({"WardRetention", "Endurance", "EnduranceThreshold"}) do
+	for _, stat in ipairs({"WardRetention", "Endurance", "EnduranceThreshold", "WardPerSecond", "WardDecayThreshold"}) do
 		output[stat] = round(calcLib.val(modDB, stat))
 		if breakdown then
 			breakdown[stat] = breakdown.simple(nil, nil, output[stat], stat)
