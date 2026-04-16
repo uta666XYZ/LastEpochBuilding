3f407feb714cb1711b6b57e7968f66a8167e6452
uta666XYZ <17289715+uta666XYZ@users.noreply.github.com>
2026-04-16 20:45:37 +1200

    feat: add NearEnemy condition for 'from nearby enemies' / 'while near an enemy' mods

--- src/Modules/ConfigOptions.lua
+++ src/Modules/ConfigOptions.lua
@@ -269,6 +269,9 @@ local options = {
 	{ var = "multiplierPoisonOnSelf", type = "count", label = "# of Poison on You:", ifMult = "PoisonStack", implyCond = "Poisoned", tooltip = "This also implies that you are Poisoned.", apply = function(val, modList, enemyModList)
 		modList:NewMod("Multiplier:PoisonStack", "BASE", val, "Config", { type = "Condition", var = "Effective" })
 	end },
+	{ var = "conditionNearEnemy", type = "check", label = "Are you Near an Enemy?", ifCond = "NearEnemy", apply = function(val, modList, enemyModList)
+		modList:NewMod("Condition:NearEnemy", "FLAG", true, "Config", { type = "Condition", var = "Combat" })
+	end },
 	{ var = "multiplierNearbyEnemies", type = "count", label = "# of nearby Enemies:", ifMult = "NearbyEnemies", apply = function(val, modList, enemyModList)
 		modList:NewMod("Multiplier:NearbyEnemies", "BASE", val, "Config", { type = "Condition", var = "Combat" })
 		modList:NewMod("Condition:OnlyOneNearbyEnemy", "FLAG", val == 1, "Config", { type = "Condition", var = "Combat" })
